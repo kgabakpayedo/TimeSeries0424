e746c064d28bc482efbc27eaf22a340f3562452e
kgabakpayedo <kokouelvismondesir.gaba-kpayedo@ensae.fr>
2024-05-06 21:17:28 +0000

    Stationnarity test, order guessing and validation, Residuals testing and forecasting

--- scripts/02-Stationnarity.r
+++ scripts/02-Stationnarity.r
@@ -1,42 +1,37 @@
 # Loading the packages to run statistical operations on the series
 
 library(tseries)
+library(forecast)
 
+
+
+# Function to check stationnarity by performing ADF and KPSS test and plotting the time series 
 
 # Considering the first-order and second-order differences
 
-diff_tseries <- na.omit(diff(tseries))
-
-two_diff_tseries <- na.omit(diff(diff_tseries))
 
 
-# Making a seasonal trend and residuals decomposition and printing the 3 components together
+first_diff_tseries <- na.omit(diff(log(tseries)))
 
-dec_tseries <- decompose(tseries)
+dygraph(first_diff_tseries)
 
-trend <- dec_tseries$trend
-seasonal <- dec_tseries$seasonal
-random <- dec_tseries$random
-
- 
-
-trend_graph <- dygraph(trend)
-seasonal_graph <- dygraph(seasonal)
-random_graph <- dygraph(random)
+print(adf.test(first_diff_tseries))
+print(kpss.test(first_diff_tseries))
 
 
-print(trend_graph)
-print(seasonal_graph)
-print(random_graph)
+two_diff_tseries <- na.omit(diff(tseries, differences = 2))
 
-nf <- layout(matrix(c(1:3),3,1, byrow = TRUE))
-layout.show(nf)
+dygraph(two_diff_tseries)
 
-
+print(adf.test(two_diff_tseries))
+print(kpss.test(two_diff_series))
 
 
-# Performing the ADF test to check stationnarity
+# Considering the seasonal difference of the 12-th lag
 
-adf_result <- adf.test(tseries)
+seasonal_diff_tseries <- na.omit(diff(tseries, lag = 12))
 
-print(adf_result)
+dygraph(seasonal_diff_tseries)
+
+print(adf.test(seasonal_diff_tseries))
+print(kpss.test(seasonal_diff_tseries))
